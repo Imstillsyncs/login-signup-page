60748a3ec11a3cbf214b861a7ce42df60dfa66ad
Thomas Robinson <89710138+Imstillsyncs@users.noreply.github.com>
2022-04-28 12:02:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var email = '';
+var password = '';
+
+function signup() {
+	if (document.getElementById('agree').checked == true) {
+		email = document.getElementById('email').value;
+		password = document.getElementById('password').value;
+	} else {
+		p = document.createElement('P');
+		p.innerHTML = 'You need to agree to the terms and conditions.';
+		document.body.appendChild(p);
+	}
+}
+
+function login() {
+	confirmEmail = document.getElementById('mail').value;
+	confirmPass = document.getElementById('password').value;
+	if (confirmEmail == email && confirmPass == password) {
+		p = document.createElement('P');
+		p.innerHTML = 'You successfully logged in to your account!';
+		document.body.appendChild(p);
+	} else {
+		p = document.createElement('P');
+		p.innerHTML = 'Incorrect email or password';
+		document.body.appendChild(p);
+	}
+}
